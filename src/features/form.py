e6263e8f59213fc6671c7abc0dccd832e0f7969c
NoamSohar <il.noamzohar@gmail.com>
2026-08-15 19:41:54 +0300

    Form Calculator
    Added Form Calculator class,
    a function to check how was the team doing in their last 5 matches home/away.
    implemented a function that adds these 2 features into the database.

---
+++ src/features/form.py
@@ -0,0 +1,63 @@
+import pandas as pd
+
+class FormCalculator:
+    def __init__(self, matches: pd.DataFrame, last_games: int = 5):
+        self.matches = matches
+        self.last_games = last_games
+
+    def get_team_form(self, team_id: int, match_date):
+        previous_matches = self.matches[
+            (self.matches["date"] < match_date)
+            &
+            (
+                (self.matches["home_team_id"] == team_id)
+                |
+                (self.matches["away_team_id"] == team_id)
+            )
+        ]
+
+        if len(previous_matches) < self.last_games:
+            return None
+
+        previous_matches = previous_matches.tail(self.last_games)
+
+        points = 0
+        for _, match in previous_matches.iterrows():
+            is_home = match["home_team_id"] == team_id
+
+            if match["home_goals"] == match["away_goals"]:
+                points += 1
+
+            elif is_home and match["home_goals"] > match["away_goals"]:
+                points += 3
+
+            elif not is_home and match["away_goals"] > match["home_goals"]:
+                points += 3
+
+        return points / self.last_games
+
+    def add_features(self) -> pd.DataFrame:
+        df = self.matches.copy()
+
+        home_forms = []
+        away_forms = []
+
+        for _, match in df.iterrows():
+            home_forms.append(
+                self.get_team_form(
+                    match["home_team_id"],
+                    match["date"]
+                )
+            )
+
+            away_forms.append(
+                self.get_team_form(
+                    match["away_team_id"],
+                    match["date"]
+                )
+            )
+
+        df[f"home_form_{self.last_games}"] = home_forms
+        df[f"away_form_{self.last_games}"] = away_forms
+
+        return df
